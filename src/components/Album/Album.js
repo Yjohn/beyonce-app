0bae54ed04aea1de0c9b37345272b2ed6f4c39c9
YJohn <yfekadu2016@gmail.com>
2017-10-16 10:45:15 +0100

    create a beyonce app with react

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import './Album.css';
+import ReactPlayer from 'react-player'
 
 const Album = props => (
   <div>
-    <h2>{props.album}</h2>
-    <img src={props.imageUrl} />
-    <p>Release date: {props.releaseDate}</p>
-    <p>Genre: {props.genre}</p>
+    <section class = "container">
+      <h2>{props.album}</h2>
+      <img src={props.imageUrl} />
+      <p>Release date: {props.releaseDate}</p>
+      <p>Genre: {props.genre}</p>
+      {/* {<iframe width='500px' height='500px' src={props.url} >link</iframe>} */}
+      {<ReactPlayer url={props.url} />}
+    </section>
   </div>
 );
 
@@ -18,4 +23,4 @@ Album.propTypes = {
   genre: propTypes.string.isRequired
 };
 
-export default App;
+export default Album;
--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
+import Album from '../Album/Album';
+import '../App/App.css';
 
 class Albums extends Component {
+  constructor() {
+    super();
+    this.state = {
+      albums: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/rarmatei/f5ae92ac93d9716affab822a3f54f95b/raw/e62641b3f5ddd12c4fe34aa0912488224594e5a7/beyonce-albums.json')
+      .then(response => response.json())
+      .then(this.updateAlbums)
+  }
+  updateAlbums = (data) => {
+    this.setState({
+      albums: data
+    })
+  }
   render() {
     return (
-      <div></div>
-    )
+      <div>
+        <section>
+          <h1>Albums</h1>
+          {this.state.albums.map(function (album) {
+            return (
+              <Album
+                key={album.albumId}
+                album={album.collectionName}
+                artist={album.artistName}
+                imageUrl={album.artworkUrl100}
+                releaseDate={album.releaseDate}
+                genre={album.primaryGenreName}
+                url={album.url} />
+            )
+          })}
+        </section>
+        {/* <div>{JSON.stringify(this.state.albums)}</div> */}
+      </div>
+    );
   }
 }
+// Albums.propTypes = {
 
-Albums.propTypes = {
-
-};
+// };
 
 export default Albums;
